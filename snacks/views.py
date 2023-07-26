858f9f21a3234ca33002bc294a86d5346b7abc77
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-07-26 18:21:19 +0000

    d

---
+++ snacks/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .models import Thing ,Post
+from rest_framework.generics import ListAPIView, RetrieveAPIView,ListCreateAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from .serializers import ThingSerializer
+
+from .serializers import ThingSerializer ,PostSerializer
+
+from rest_framework.permissions import AllowAny
+# Create your views here.
+
+
+class ThingListView(ListCreateAPIView):
+    
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+
+class ThingDetailView(RetrieveUpdateDestroyAPIView):  
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer  
+
+
+
+class PostListView(ListCreateAPIView): 
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes= [AllowAny]
+    
+class PostDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes= [AllowAny]
